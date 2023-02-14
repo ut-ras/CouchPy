21f967d41805b49d6454d146760ca877c5574632
Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
2023-02-14 14:14:36 -0600

    moving average control

--- control.py
+++ control.py
@@ -1,30 +1,44 @@
 import multiprocessing as mp
-import threading as thr
 import time
-# from sabertooth2x60 import SabertoothPacketized
-import numpy as np
-import dataplotting
+# import motor
+import sim_motor as motor
 from collections import deque
 
 class ControlThread():
-
-    # TODO: PID
 
     def __init__(self, buffer:mp.Array, queue:mp.JoinableQueue, port='dev/ttyS0', *args, **kwargs):
         self.buf = buffer
         self.q = queue
         self.port = port
-        # self.motor_controllers = SabertoothPacketized(port, baudrate=9600)
+        self.motor_controller = motor.MotorController(port)
+
+        self.control_l = ControlThread.ControlLoop((10, 10))
+        self.control_r = ControlThread.ControlLoop((10, 10))
         self.buf_idx = kwargs.get('buf_idx', None)
         self.period = kwargs.get('period', 0.02)
 
-    def _calculate_next_speed(self):
-        pass
+    class ControlLoop():
+        """
+        Multi-order moving average filter
+        """
+
+        def __init__(self, layer_sizes:tuple[int]):
+            self.layer_sizes = layer_sizes
+            # layers[0] is first moving average, [1] is moving average of [0], etc.
+            self.layers = [deque([0.0 for i in range(l)], l) 
+                           for l in layer_sizes]
+            self.totals = [0.0 for _ in layer_sizes]
+
+        # setpoint should be a value between (-100.0 and 100.0) but any value will work
+        def update(self, setpoint:float) -> float:
+            prev_average = setpoint
+            for i, l in enumerate(self.layers):
+                self.totals[i] = self.totals[i] - l[0] + prev_average
+                l.append(prev_average)
+                prev_average = self.totals[i]/self.layer_sizes[i]
+            return prev_average
 
     def run(self):
-
-        self.deque = deque([0.0 for _ in range(200)], 200)
-        self.plot = dataplotting.DataPlotting({'motor':self.deque}, {'motor':(191/256,87/256,0/256)})
 
         # funny little function to sleep for (period - execution time)
         def get_sleep_time():
@@ -37,10 +51,12 @@ class ControlThread():
         while True:
             time.sleep(next(sleep_time))
 
-            speed = 0.0 # TODO:
-            self.deque.append(speed)
-            self.plot.update()
-
-            # self.motor_controllers.motors_set_right(speed_l)
-            # self.motor_controllers.motors_set_left(speed_r)
+            speed_l = self.control_l.update(
+                    float(self.buf[self.buf_idx['L-U/D']]))
+            speed_r = self.control_r.update(
+                    float(self.buf[self.buf_idx['R-U/D']]))
+            
+            self.motor_controller.motors_set_left(speed_l)
+            self.motor_controller.motors_set_right(speed_r)
+            
 
---
+++ motor.py
@@ -0,0 +1,10 @@
+from sabertooth2x60 import SabertoothPacketized
+
+class MotorController():
+
+    def __init__(self, port, *args, **kwargs):
+        self.motor_controller = SabertoothPacketized(port, baudrate=9600)
+    def motors_set_right(self, speed):
+        self.motor_controller.motors_set_right(speed)
+    def motors_set_left(self, speed):
+        self.motor_controller.motors_set_right(speed)
